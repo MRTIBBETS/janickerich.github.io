f8357ca720262662d66aac9dbc1559ae93e59f6f
Alexander Tibbets <alexander.tibbets@gmail.com>
2026-06-04 16:42:54 -0700

    Optimize website performance, PWA capabilities, and LLM discoverability

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-npx --no-install html-minifier \
+set -euo pipefail
+
+# Update sitemap lastmod date to current build date
+CURRENT_DATE=$(date +%Y-%m-%d)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' "s|<lastmod>[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}</lastmod>|<lastmod>${CURRENT_DATE}</lastmod>|g" sitemap.xml
+else
+  sed -i "s|<lastmod>[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}</lastmod>|<lastmod>${CURRENT_DATE}</lastmod>|g" sitemap.xml
+fi
+
+# Run HTML Minification with html-minifier-terser
+npx --no-install html-minifier-terser \
   --collapse-whitespace \
   --remove-comments \
   --remove-optional-tags \
